010ab7fab1ed04d8ee992412713e3c1db70f4a68
yandapallisai <121351295+yandapallisai@users.noreply.github.com>
2025-04-22 07:24:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -846,3 +846,5 @@ def get_students_performance():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
