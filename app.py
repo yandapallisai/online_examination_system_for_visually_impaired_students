36643ea0fd16797b7dba6a8d0391b81f7f76ca0f
yandapallisai <121351295+yandapallisai@users.noreply.github.com>
2025-04-22 10:34:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -846,5 +846,6 @@ def get_students_performance():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT not set
+    port = int(os.environ.get("PORT", 5000))
+    print(f"🚀 Starting Flask on port {port}")
     app.run(host="0.0.0.0", port=port)
